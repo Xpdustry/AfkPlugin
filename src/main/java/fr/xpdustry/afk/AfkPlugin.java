563321ba639c63817abbd670c646fcab53719e6a
Phinner <62483793+Phinner@users.noreply.github.com>
2021-10-01 18:35:45 +0200

    1.3 update, for external usage

--- src/main/java/fr/xpdustry/afk/AfkPlugin.java
+++ src/main/java/fr/xpdustry/afk/AfkPlugin.java
@@ -4,59 +4,44 @@ import arc.*;
 import arc.struct.*;
 import arc.util.*;
 
+import mindustry.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.game.EventType.*;
 
-import static arc.util.Log.*;
-import static mindustry.Vars.*;
+import java.util.*;
 
 
 @SuppressWarnings("unused")
 public class AfkPlugin extends Plugin{
     // Settings
-    public static String message = "[scarlet]You have been kicked for being AFK.";
-    public static boolean enabled = true;
-    public static int maxAfkPeriod = 10;
-    
-    public static final ObjectMap<Player, PosWatcher> kicker = new ObjectMap<>();
-    public static final Interval updater = new Interval();
+    private static String message = "[scarlet]You have been kicked for being AFK.";
+    private static boolean enabled = true;
+    private static int duration = 10;
+    // Internals
+    private static final Interval updater = new Interval();
+    private static final HashSet<AfkWatcher> kicker = new HashSet<>(8);
 
     @Override
+    @SuppressWarnings("all")
     public void init(){
         // Load the settings
-        if(Core.settings.has("xpdustry-afk-message")) message = Core.settings.getString("xpdustry-afk-message");
-        if(Core.settings.has("xpdustry-afk-enabled")) enabled = Core.settings.getBool("xpdustry-afk-enabled");
-        if(Core.settings.has("xpdustry-afk-duration")) maxAfkPeriod = Core.settings.getInt("xpdustry-afk-duration");
+        message = Core.settings.getString("xpdustry-afk-message", message);
+        enabled = Core.settings.getBool("xpdustry-afk-enabled", enabled);
+        duration = Core.settings.getInt("xpdustry-afk-duration", duration);
 
         // Deploy the listeners
-        Events.on(PlayerJoin.class, e -> kicker.put(e.player, new PosWatcher()));
+        Events.on(PlayerJoin.class, e -> kicker.add(new AfkWatcher(e.player)));
         Events.on(PlayerLeave.class, e -> kicker.remove(e.player));
-        // The fun part :^)
         Events.run(Trigger.update, () -> {
             // Don't need to iterate every tick, every second is enough...
             if(enabled && updater.get(Time.toSeconds)){
-                kicker.each((player, watcher) -> {
-                    if (!watcher.isAFK(player)) return;
-                    player.kick(Strings.format(message, maxAfkPeriod));
+                kicker.forEach(watcher -> {
+                    if(!watcher.isAfk(duration));
+                    else watcher.getPlayer().kick(Strings.format(message, duration));
                 });
             }
         });
-    }
-
-    /** A player is considered AFK if he is not moving and not building something */
-    private static class PosWatcher{
-        /** Packed coordinates */
-        private int lastPos = 0;
-        private final Interval timer = new Interval();
-
-        private boolean isAFK(Player player){
-            int currentPos = (player.tileY() * world.height()) + player.tileX();
-            if(currentPos != lastPos || player.unit().isBuilding()) timer.reset(0, 0);
-
-            lastPos = currentPos;
-            return timer.get(maxAfkPeriod * Time.toMinutes);
-        }
     }
 
     @Override
@@ -66,41 +51,111 @@ public class AfkPlugin extends Plugin{
             switch (args[0].toLowerCase()){
                 case "time":
                     if(args.length == 1){
-                        info("The current maximum afk time is @ minute@.", maxAfkPeriod, (maxAfkPeriod > 1 ? "s" : ""));
+                        Log.info("The current maximum afk time is @ minute@.", duration, (duration > 1 ? "s" : ""));
                     }else if(Strings.canParsePositiveInt(args[1])){
-                        maxAfkPeriod = Integer.parseInt(args[1]);
-                        info("The new maximum afk period is @ minute@.", maxAfkPeriod, (maxAfkPeriod > 1 ? "s" : ""));
-                        Core.settings.put("xpdustry-afk-duration", maxAfkPeriod);
+                        setDuration(Integer.parseInt(args[1]));
                     }else{
-                        err("You inputted an invalid number.");
+                        Log.err("You inputted an invalid number.");
                     }
 
                     break;
 
                 case "status":
                     if(args.length == 1){
-                        info("The AFK kicker is @.", enabled ? "enabled" : "disabled");
+                        Log.info("The AFK kicker is @.", enabled ? "enabled" : "disabled");
                     }else{
-                        enabled = args[1].equalsIgnoreCase("true") || args[1].equalsIgnoreCase("enabled");
-                        info("The AFK kicker is now @.", enabled ? "enabled" : "disabled");
-                        Core.settings.put("xpdustry-afk-enabled", enabled);
+                        setEnabled(args[1].equalsIgnoreCase("true") || args[1].equalsIgnoreCase("enabled"));
                     }
 
                     break;
 
                 case "message":
                     if(args.length == 1){
-                        info("The current kick message is '@'", message);
+                        Log.info("The current kick message is '@'", message);
                     }else{
-                        message = args[1];
-                        info("The current kick message is '@'", message);
-                        Core.settings.put("xpdustry-afk-message", message);
+                        setMessage(args[1]);
                     }
 
                     break;
 
-                default: info("Your command is invalid.");
+                default: Log.info("Your command is invalid.");
             }
         });
+    }
+
+    public static String getMessage(){
+        return message;
+    }
+
+    public static void setMessage(String message){
+        AfkPlugin.message = message;
+        Log.info("The current kick message is '@'", message);
+        Core.settings.put("xpdustry-afk-message", message);
+    }
+
+    public static boolean isEnabled(){
+        return enabled;
+    }
+
+    public static void setEnabled(boolean enabled){
+        AfkPlugin.enabled = enabled;
+        Log.info("The AFK kicker is now @.", enabled ? "enabled" : "disabled");
+        Core.settings.put("xpdustry-afk-enabled", enabled);
+    }
+
+    public static int getDuration(){
+        return duration;
+    }
+
+    public static void setDuration(int duration){
+        AfkPlugin.duration = duration;
+        Log.info("The new maximum afk period is @ minute@.", duration, (duration > 1 ? "s" : ""));
+        Core.settings.put("xpdustry-afk-duration", duration);
+    }
+
+    public static Seq<AfkWatcher> getAfkPlayers(){
+        return Seq.with(kicker);
+    }
+
+    /** A player is considered AFK if he is not moving and not building something */
+    public static class AfkWatcher{
+        /** Packed coordinates */
+        private int lastPos = 0;
+        private final Interval timer = new Interval();
+        private final Playerc player;
+
+        public AfkWatcher(Playerc player){
+            this.player = player;
+        }
+
+        public boolean isAfk(float duration){
+            int currentPos = (player.tileY() * Vars.world.height()) + player.tileX();
+            if(currentPos != lastPos || player.unit().isBuilding()) resetAfkTimer();
+
+            lastPos = currentPos;
+            return timer.get(duration * Time.toMinutes);
+        }
+
+        public void resetAfkTimer(){
+            timer.reset(0, 0);
+        }
+
+        public Playerc getPlayer(){
+            return player;
+        }
+
+        public float getAfkTime(){
+            return timer.getTime(0);
+        }
+
+        @Override
+        public int hashCode(){
+            return player.hashCode();
+        }
+
+        @Override
+        public String toString(){
+            return player.toString();
+        }
     }
 }
